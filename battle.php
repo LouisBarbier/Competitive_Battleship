c5a915b4490eee251453eaf421f9f83d6ba3d86a
LouisBarbier <barbierlouis86@gmail.com>
2024-10-31 19:29:03 -0400

    footers and headers are finally at equal distances from the corners

--- battle.php
+++ battle.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fighting {player_name} - Competitive Battleship</title>
+    <link rel="icon" type="image/svg+xml" href="images\logos\competitive_battleship.svg">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="battling_mode.css">
 </head>
 <body>
     <!--Here players will be able to battle against each other a javascript loop while check for updates (opponent responce) every n seconds-->
 
-    <header class="d-flex flex-wrap justify-content-center py-4 mb-4 border-bottom">
+    <header class="d-flex flex-wrap justify-content-center border-bottom">
         <a href="index.php" target="_self" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
             <img src="images/logos/competitive_battleship.svg" alt="Logo" class="bi me-2" width="40" height="40">
             <span class="fs-4">Competitive Battleship</span>
--- script.js
+++ script.js
@@ -4,5 +4,6 @@ document.getElementById('login').addEventListener('click',  function (event) {
       "login.html",
       "location=yes,height=570,width=520,scrollbars=yes,status=yes"
     );
+    console.log('test');
     
 });
